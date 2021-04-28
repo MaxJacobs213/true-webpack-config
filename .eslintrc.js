6013fd12f0b3de49c6f6fd4c85ac3303af150d99
Max <yakubov@yelk.com.ua>
2021-04-28 15:05:51 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,17 @@ module.exports = {
   parser: 'babel-eslint',
   extends: 'airbnb-base',
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ?  ["error", { allow: ["warn", "error"] }] : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'import/prefer-default-export': 'off',
     'arrow-parens': [2, 'as-needed', { 'requireForBlockBody': true }],
+    'no-param-reassign': [2, { "props": false }],
+    'no-new': 0,
+  },
+  env: {
+    browser: true,
+  },
+  globals: {
+    __DEV__: 'readonly',
   },
 };
